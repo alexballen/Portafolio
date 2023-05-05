19c0f4234649a9ff97c2ff906f2e44ccfd3c3375
Alex <alexballen53@gmail.com>
2023-05-04 20:48:46 -0500

    Push

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 import s from "./Home.module.css";
 
 const Home = () => {
@@ -13,41 +14,37 @@ const Home = () => {
       <main className={s.container}>
         <header className={s.container__title}>
           <h1>Portafolio</h1>
-          <div className={s.container}>
-            <div className={s.hamburger} onClick={toggleMenu}>
-              {/* <div className={s.hamburger__line}></div>
-              <div className={s.hamburger__line}></div>
-              <div className={s.hamburger__line}></div> */}
-              <MenuIcon />
+          <nav className={s.hamburger} onClick={toggleMenu}>
+            {isOpen ? (
+              <MenuOpenIcon fontSize="large" className={s.icon} />
+            ) : (
+              <MenuIcon fontSize="large" />
+            )}
+          </nav>
+          <section className={`${s.menu} ${isOpen ? s.show : ""}`}>
+            <div className={s.menu__item}>
+              <a className={s.menu__link} href="#home">
+                Home
+              </a>
             </div>
-            <div className={`${s.menu} ${isOpen ? s.show : ""}`}>
-              <div className={s.menu__item}>
-                <a className={s.menu__link} href="#home">
-                  Home
-                </a>
-              </div>
-              <div className={s.menu__item}>
-                <a className={s.menu__link} href="#about">
-                  About
-                </a>
-              </div>
-              <div className={s.menu__item}>
-                <a className={s.menu__link} href="#services">
-                  Services
-                </a>
-              </div>
-              <div className={s.menu__item}>
-                <a className={s.menu__link} href="#contact">
-                  Contact
-                </a>
-              </div>
+            <div className={s.menu__item}>
+              <a className={s.menu__link} href="#about">
+                About
+              </a>
             </div>
-          </div>
+            <div className={s.menu__item}>
+              <a className={s.menu__link} href="#services">
+                Services
+              </a>
+            </div>
+            <div className={s.menu__item}>
+              <a className={s.menu__link} href="#contact">
+                Contact
+              </a>
+            </div>
+          </section>
         </header>
-        <section className={s.menu__lateral}>
-          <div></div>
-        </section>
-        <section className={s.contenido}></section>
+        <div className={s.slide}></div>
       </main>
     </>
   );
