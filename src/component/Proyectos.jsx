fb38f28f0a67694b9571812ce26264940556e497
Alex <alexballen53@gmail.com>
2023-05-11 20:31:13 -0500

    Push

--- src/component/Proyectos.jsx
+++ src/component/Proyectos.jsx
@@ -4,8 +4,8 @@ import s from "./Proyectos.module.css";
 
 const Proyectos = () => {
   const images = [
-    { image: pi, alt: "proyecto individual", text: "Proyecto Individual" },
     { image: pf, alt: "proyecto final", text: "Proyecto Final" },
+    { image: pi, alt: "proyecto individual", text: "Proyecto Individual" },
   ];
   return (
     <main>
@@ -18,19 +18,19 @@ const Proyectos = () => {
               </div>
             </header>
             <figure>
-              <div className={s.container__images}>
+              {/* <div className={s.container__images}>
                 {images.map((imag, index) => (
                   <div className={s.container__image} key={index}>
                     <img className={s.image} src={imag.image} alt={imag.alt} />
-                    <p className={s.image__paragraph}>{imag.text}</p>
-                    <div className={s.overlay}>
+                    <p className={s.image__text}>{imag.text}</p>
+                    <div className={s.image__overlay}>
                       <a className={s.link} href="#">
-                        Link Proyecto
+                        Visitar
                       </a>
                     </div>
                   </div>
                 ))}
-              </div>
+              </div> */}
             </figure>
           </div>
         </section>
