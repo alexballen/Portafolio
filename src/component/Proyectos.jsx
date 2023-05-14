66629a594e3d4a1f0c510ab3aa9c2815b0f1076c
Alex <alexballen53@gmail.com>
2023-05-14 15:37:42 -0500

    Mejoras

--- src/component/Proyectos.jsx
+++ src/component/Proyectos.jsx
@@ -11,20 +11,24 @@ const Proyectos = () => {
     <main>
       <div className={s.container}>
         <section>
-          <div className={s.container__proyect}>
+          <div className={s.proyect__container}>
             <header>
-              <div className={s.container__title}>
-                <p className={s.title}>Proyectos</p>
+              <div className={s.proyect__title__container}>
+                <p className={s.proyect__title}>Proyectos</p>
               </div>
             </header>
             <figure>
-              <div className={s.container__images}>
+              <div className={s.proyect__images__container}>
                 {images.map((imag, index) => (
-                  <div className={s.container__image} key={index}>
-                    <img className={s.image} src={imag.image} alt={imag.alt} />
-                    <p className={s.image__text}>{imag.text}</p>
+                  <div className={s.proyect__image__container} key={index}>
+                    <img
+                      className={s.proyect__image}
+                      src={imag.image}
+                      alt={imag.alt}
+                    />
+                    <p className={s.proyect__image__text}>{imag.text}</p>
                     <div className={s.image__overlay}>
-                      <a className={s.link} href="#">
+                      <a className={s.image__link} href="#">
                         Visitar
                       </a>
                     </div>
