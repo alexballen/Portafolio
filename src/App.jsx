9e7a399bd73313f9f197ac006b3bb082578eb098
Alex <alexballen53@gmail.com>
2023-05-15 22:02:36 -0500

    Push

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Tecnologias from "./component/Tecnologias";
 import AcercaDeMi from "./component/AcercaDeMi";
 import Proyectos from "./component/Proyectos";
+import Contacto from "./component/Contacto";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" element={<AcercaDeMi />} />
         <Route path="/tecnologias" element={<Tecnologias />} />
         <Route path="/proyectos" element={<Proyectos />} />
+        <Route path="/contacto" element={<Contacto />} />
       </Routes>
     </>
   );
