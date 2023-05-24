38607f6cfc5cf8ce44292e2207e171c98be0d599
Alex <alexballen53@gmail.com>
2023-05-23 19:45:20 -0500

    Se añaden animaciones y se crea la redireccion de cada uno de los links del componente Tecnologias

--- src/component/Tecnologias.jsx
+++ src/component/Tecnologias.jsx
@@ -14,19 +14,92 @@ import s from "./Tecnologias.module.css";
 
 const Tecnologias = () => {
   const icons = [
-    { image: iconhtml, alt: "icohtml", text: "HTML" },
-    { image: iconcss, alt: "icocss", text: "CSS" },
-    { image: iconjs, alt: "icojs", text: "JavaScript" },
-    { image: iconnode, alt: "iconode", text: "Node JS" },
-    { image: iconreact, alt: "icoreact", text: "React" },
-    { image: iconredux, alt: "icoredux", text: "Redux" },
-    { image: iconpostgreSQL, alt: "icopostgreSQL", text: "PostgreSQL" },
-    { image: iconsequelize, alt: "icosequelize", text: "Sequelize" },
-    { image: icongithub, alt: "icogithub", text: "GitHub" },
-    { image: iconscrum, alt: "icoscrum", text: "SCRUM" },
-    { image: iconfigma, alt: "icofigma", text: "Figma" },
-    { image: icontrello, alt: "icotrello", text: "Trello" },
+    {
+      image: iconhtml,
+      alt: "icohtml",
+      text: "HTML",
+      link: "https://developer.mozilla.org/es/docs/Web/HTML",
+      target: "_blank",
+    },
+    {
+      image: iconcss,
+      alt: "icocss",
+      text: "CSS",
+      link: "https://www.w3schools.com/css/",
+      target: "_blank",
+    },
+    {
+      image: iconjs,
+      alt: "icojs",
+      text: "JavaScript",
+      link: "https://developer.mozilla.org/es/docs/Web/JavaScript",
+      target: "_blank",
+    },
+    {
+      image: iconnode,
+      alt: "iconode",
+      text: "Node JS",
+      link: "https://nodejs.org/es",
+      target: "_blank",
+    },
+    {
+      image: iconreact,
+      alt: "icoreact",
+      text: "React",
+      link: "https://es.react.dev/",
+      target: "_blank",
+    },
+    {
+      image: iconredux,
+      alt: "icoredux",
+      text: "Redux",
+      link: "https://es.redux.js.org/",
+      target: "_blank",
+    },
+    {
+      image: iconpostgreSQL,
+      alt: "icopostgreSQL",
+      text: "PostgreSQL",
+      link: "https://www.postgresql.org/",
+      target: "_blank",
+    },
+    {
+      image: iconsequelize,
+      alt: "icosequelize",
+      text: "Sequelize",
+      link: "https://sequelize.org/",
+      target: "_blank",
+    },
+    {
+      image: icongithub,
+      alt: "icogithub",
+      text: "GitHub",
+      link: "https://github.com/",
+      target: "_blank",
+    },
+    {
+      image: iconscrum,
+      alt: "icoscrum",
+      text: "SCRUM",
+      link: "https://www.scrum.org/resources/blog/que-es-scrum",
+      target: "_blank",
+    },
+    {
+      image: iconfigma,
+      alt: "icofigma",
+      text: "Figma",
+      link: "https://www.figma.com/",
+      target: "_blank",
+    },
+    {
+      image: icontrello,
+      alt: "icotrello",
+      text: "Trello",
+      link: "https://trello.com/",
+      target: "_blank",
+    },
   ];
+
   return (
     <main>
       <div className={s.container}>
@@ -34,20 +107,20 @@ const Tecnologias = () => {
           <div className={s.technologies__container}>
             <header>
               <div className={s.technologies__title__container}>
-                <p id="tecnologias" className={s.technologies__title}>
-                  Tecnologias
-                </p>
+                <p className={s.technologies__title}>Tecnologias</p>
               </div>
             </header>
             <figure>
               <div className={s.technologies__icons__container}>
                 {icons.map((icon, index) => (
                   <div className={s.technologies__icon__container} key={index}>
-                    <img
-                      className={s.technologies__icon}
-                      src={icon.image}
-                      alt={icon.alt}
-                    />
+                    <a href={icon.link} target={icon.target}>
+                      <img
+                        className={s.technologies__icon}
+                        src={icon.image}
+                        alt={icon.alt}
+                      />
+                    </a>
                     <p>{icon.text}</p>
                   </div>
                 ))}
