c933c3db1b2109667d3375a3264df88a062a3314
Alex <alexballen53@gmail.com>
2023-05-25 20:21:02 -0500

    Se modifica el componente DarkMode y se le dan nuevos estilos y animacion

--- src/component/DarkMode.jsx
+++ src/component/DarkMode.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import DarkModeContext from "./DarkModeContext";
 import { IconContext } from "react-icons";
 import { MdOutlineLightMode, MdModeNight } from "react-icons/md";
+import s from "./DarkMode.module.css";
 
 const DarkMode = () => {
   const { darkMode, setDarkMode } = useContext(DarkModeContext);
@@ -13,16 +14,16 @@ const DarkMode = () => {
   return (
     <main>
       <section>
-        <div onClick={handleDarkMode}>
+        <div onClick={handleDarkMode} className={s.container}>
           {!darkMode ? (
             <IconContext.Provider
-              value={{ className: "shared-class", size: 40, color: "black" }}
+              value={{ className: "shared", size: 25, color: "#372a2af6" }}
             >
               <MdModeNight />
             </IconContext.Provider>
           ) : (
             <IconContext.Provider
-              value={{ className: "shared", size: 40, color: "white" }}
+              value={{ className: "shared", size: 25, color: "#f1da0d" }}
             >
               <MdOutlineLightMode />
             </IconContext.Provider>
