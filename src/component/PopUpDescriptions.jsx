a40ed73592323f6f10328ab26c2771aa4693f72a
Alex <alexballen53@gmail.com>
2023-08-19 15:08:42 -0500

    Se crea la logica para crear un popup, se importan iconos

--- src/component/PopUpDescriptions.jsx
+++ src/component/PopUpDescriptions.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import { TfiHelpAlt } from "react-icons/tfi";
+import { SlClose } from "react-icons/sl";
 import s from "./PopUpDescriptions.module.css";
 
 const PopUpDescriptions = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const openPopup = () => {
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
+
   return (
     <main>
-      <div className={s.container}>
-        <h1>PopUpDescriptions</h1>
+      <div className={s.help_container}>
+        <TfiHelpAlt onClick={openPopup} className={s.TfiHelpAlt} />
+      </div>
+      <div className={`${s.popup} ${showPopup ? s.show : ""}`}>
+        <div className={s.closed_container}>
+          <SlClose className={s.closed} onClick={closePopup} />
+        </div>
+        {/*  <Component /> */}
       </div>
     </main>
   );
