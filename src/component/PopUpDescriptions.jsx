1cbfe221f2d54471c880f623632578afa794c9df
Alex <alexballen53@gmail.com>
2023-08-18 20:20:15 -0500

    Se crea componente para mostrar la descripcion de cada uno de los proyectos

---
+++ src/component/PopUpDescriptions.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from "./PopUpDescriptions.module.css";
+
+const PopUpDescriptions = () => {
+  return (
+    <main>
+      <div className={s.container}>
+        <h1>PopUpDescriptions</h1>
+      </div>
+    </main>
+  );
+};
+
+export default PopUpDescriptions;
