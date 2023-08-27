6508ea709c47224052eef87de2398a54daa15e76
Alex <alexballen53@gmail.com>
2023-08-26 20:01:08 -0500

    Se modifica el contenido del la aplicacion PortafolioParagraph

--- src/component/PortafolioParagraph.jsx
+++ src/component/PortafolioParagraph.jsx
@@ -6,18 +6,13 @@ const PortafolioParagraph = () => {
       <div className={s.container}>
         <section>
           <div className={s.title_container}>
-            <p>Web App Anotalo</p>
+            <p>Web App Portafolio</p>
           </div>
         </section>
         <section>
           <div className={s.paragraph_container}>
             <p className={s.introductory_paragraph}>
-              Anótalo es una aplicación web diseñada para simplificar tu vida
-              mediante la creación y gestión de notas. Esta intuitiva aplicación
-              te permite crear y organizar tus notas de manera eficiente,
-              asignándoles recordatorios personalizados y clasificándolas según
-              su importancia, todo bajo tu criterio único. Además, cuenta con
-              funciones de filtros combinados y búsqueda por título.
+              Portafolio es una aplicación web ...
             </p>
             <p>
               La aplicación cuenta con un diseño totalmente adaptable
@@ -25,26 +20,11 @@ const PortafolioParagraph = () => {
             </p>
             <p>Tecnologías utilizadas:</p>
             <p>
-              Frontend: Vite, React, Redux, dayjs, lodash, react-icons,
-              react-tooltip, sweetalert2, date-fns, react-hook-form.
+              Frontend: Vite, React, react-icons, react-tooltip,
+              sweetalert2,react-hook-form.
             </p>
-            <p>Backend: JWT, bcryptjs, dayjs, ejs, nodemailer, express.</p>
             <p>Características destacadas de la aplicación:</p>
-            <p>- Autenticación local y a través de Google.</p>
-            <p>- Notificaciones para nuevos usuarios.</p>
-            <p>- Validación de cuentas de nuevos usuarios.</p>
-            <p>- Recuperación de contraseña.</p>
-            <p>- Perfil de usuario.</p>
-            <p>- Recordatorios para notas.</p>
-            <p>
-              - Notificaciones por correo electrónico para recordatorios
-              configurados.
-            </p>
-            <p>
-              - Filtros de ordenamiento A-Z y Z-A, filtrado por importancia y
-              búsqueda por título.
-            </p>
-            <p>- Edición y eliminación de notas.</p>
+            <p></p>
           </div>
         </section>
       </div>
