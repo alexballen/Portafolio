3c79e0e84dc78fba2da1a7afbfe89afc0a51ddb7
Alex <alexballen53@gmail.com>
2024-01-13 18:51:31 -0500

    Se actualiza la informacion del popup del proyecto final

--- src/component/PFParagraph.jsx
+++ src/component/PFParagraph.jsx
@@ -12,15 +12,32 @@ const PFParagraph = () => {
         <section>
           <div className={s.paragraph_container}>
             <p className={s.introductory_paragraph}>
-              Ecommerce es una aplicación web
+              Desarrollo de una página web de Comercio Electrónico especializada
+              en productos de alta costura.
+            </p>
+            <p>
+              La página web cuenta con autenticación de usuarios,
+              funcionalidades de filtrado y búsqueda de productos, pasarela de
+              pago, un completo dashboard, entre otras características.
+            </p>
+            <p>
+              La aplicación fue desarrollada de forma colaborativa utilizando la
+              metodología SCRUM, Git Flow y una implementación eficiente de
+              tiempos, objetivos y comunicación para cumplir con los plazos
+              estimados.
+            </p>
+            <p>
+              Se emplearon diversas tecnologías que se integraron de manera
+              armoniosa para lograr el resultado deseado.
             </p>
             <p>Tecnologías utilizadas:</p>
-            <p>Frontend: React, Redux, sweetalert2.</p>
-            <p>Backend: </p>
-            <p>Características destacadas de la aplicación:</p>
             <p>
-              - Filtros de ordenamiento A-Z y Z-A, filtrado por importancia y
-              búsqueda por título.
+              Frontend: React, auth0-react, Redux, sweetalert2, daisyui,
+              Firebase, react-icons, react-simple-chatbot, uuid, entre otros.
+            </p>
+            <p>
+              Backend: Node, auth0-js, Express, MercadoPago, Nodemailer, PG,
+              Sequelize, Socket.io.
             </p>
           </div>
         </section>
